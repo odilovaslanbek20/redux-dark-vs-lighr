de49ed3d976d424ec3c8d9ac86a7a0b754689293
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-04-11 20:33:01 +0500

    "hhhh"

--- src/pages/Chiroq.jsx
+++ src/pages/Chiroq.jsx
@@ -7,16 +7,49 @@ function Chiroq() {
 	return (
 		<>
 			<section className='w-100 h-20 border rounded-xl p-2'>
-				<div className={`w-full h-full border rounded-[8px] ${isDarkMode ? "bg-[#fff]" : "bg-[#000000]"} flex items-center justify-center`}>
-					<p className={` ${isDarkMode ? "text-black" : "text-white"} text-xl font-semibold`}>
-						Chiroq <span className='text-blue-500'>{isDarkMode ? "Yoqildi" : "O'chirildi"}</span> 💡
+				<div
+					className={`w-full h-full border rounded-[8px] ${
+						isDarkMode ? 'bg-[#fff]' : 'bg-[#000000]'
+					} flex items-center justify-center`}
+				>
+					<p
+						className={` ${
+							isDarkMode ? 'text-black' : 'text-white'
+						} text-xl font-semibold`}
+					>
+						Chiroq{' '}
+						<span className='text-blue-500'>
+							{isDarkMode ? 'Yoqildi' : "O'chirildi"}
+						</span>{' '}
+						💡
 					</p>
 				</div>
 			</section>
 			<div className='flex items-center gap-2'>
-				<p onClick={() => dispatch({type: "LIGHT"})} className='bg-[#fff] px-3.5 py-1 text-black rounded cursor-pointer'>Light</p>
-				<p onClick={() => dispatch({type: "TOGGLE"})} className='bg-[#fff] px-3.5 py-1 text-black rounded cursor-pointer'>Toggle</p>
-				<p onClick={() => dispatch({type: "DARK"})} className='bg-[#fff] px-3.5 py-1 text-black rounded cursor-pointer'>Dark</p>
+				<p
+					onClick={() => dispatch({ type: 'LIGHT' })}
+					className={` ${
+						!isDarkMode ? 'text-white bg-black' : 'text-black bg-white'
+					} px-3.5 py-1  rounded cursor-pointer`}
+				>
+					Light
+				</p>
+				<p
+					onClick={() => dispatch({ type: 'TOGGLE' })}
+					className={` ${
+						!isDarkMode ? 'text-white bg-black' : 'text-black bg-white'
+					} px-3.5 py-1  rounded cursor-pointer`}
+				>
+					Toggle
+				</p>
+				<p
+					onClick={() => dispatch({ type: 'DARK' })}
+					className={` ${
+						!isDarkMode ? 'text-white bg-black' : 'text-black bg-white'
+					} px-3.5 py-1 rounded cursor-pointer`}
+				>
+					Dark
+				</p>
 			</div>
 		</>
 	)
